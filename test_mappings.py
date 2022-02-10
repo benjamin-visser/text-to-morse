aad8e4e00f4b6e9b8cd9d81716a7c0cd5d5a60c7
Benjamin Visser <benjaminjamesvisser@gmail.com>
2022-02-10 10:24:27 +0200

    add tests for morse dictionary mappings

---
+++ test_mappings.py
@@ -0,0 +1,25 @@
+from morsekeys import morse_dict
+
+
+alphabet_list = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
+                 "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+
+numeral_list = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
+
+punctuation_list = ["!", "@", "$", "&", "(", ")", "-", "_", "=",
+                    "+", "'", ":", ";", ".", ",", "?", "/", "\""]
+
+
+def test_alphabet():
+    for char in alphabet_list:
+        print(morse_dict[char])
+
+
+def test_numerals():
+    for char in numeral_list:
+        print(morse_dict[char])
+
+
+def test_punctuation():
+    for char in punctuation_list:
+        print(morse_dict[char])
