3a283e7ba394f19ffd1996d80a28c901c31b7f02
Benjamin Visser <benjaminjamesvisser@gmail.com>
2022-02-10 14:51:26 +0200

    set mapping dictionaries as class attribute in MorseCoder

--- morse_coder.py
+++ morse_coder.py
@@ -3,11 +3,15 @@ from morsekeys import morse_dict
 
 class MorseCoder:
 
+    def __init__(self):
+        self.morse_mappings = morse_dict
+        self.char_mappings = {val: key for key, val in morse_dict.items()}
+
     def encode(self, text):
 
         word_list = text.split()
         nested_chars = [list(word.upper()) for word in word_list]
-        morse_chars = [[morse_dict[char] for char in word] for word in nested_chars]
+        morse_chars = [[self.morse_mappings[char] for char in word] for word in nested_chars]
         morse_words = [" ".join(word) for word in morse_chars]
         morse_text = "   ".join(morse_words)
 
@@ -15,10 +19,9 @@ class MorseCoder:
 
     def decode(self, code):
 
-        morse_dict_ivd = {val: key for key, val in morse_dict.items()}
         code_list = code.split("   ")
         nested_codes = [code.split() for code in code_list]
-        text_chars = [[morse_dict_ivd[char] for char in code] for code in nested_codes]
+        text_chars = [[self.char_mappings[char] for char in code] for code in nested_codes]
         text_words = ["".join(char) for char in text_chars]
         plain_text = " ".join(text_words)
 
