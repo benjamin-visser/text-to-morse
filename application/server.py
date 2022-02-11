700df7710ea4081599057f7ce37f33c0c8ab64f2
Benjamin Visser <benjaminjamesvisser@gmail.com>
2022-02-11 17:09:58 +0200

    add custom css style sheet replace text area input with wtf-form include google font for heading

--- application/server.py
+++ application/server.py
@@ -1,21 +1,33 @@
 from flask import Flask, render_template, request, redirect
 from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms import TextAreaField, SubmitField
 from morse_coder import MorseCoder
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "this is a temporary key"
 Bootstrap(app)
 coder = MorseCoder()
 
 
+class InputForm(FlaskForm):
+    user_input = TextAreaField()
+    encode = SubmitField("Encode")
+    undo = SubmitField("Undo")
+    decode = SubmitField("Decode")
+
+
 @app.route("/", methods=["GET", "POST"])
 def home():
-    if request.method == "POST":
-        text = request.form["user_text"]
+    form = InputForm()
+    if form.validate_on_submit():
+        text = form.user_input.data
         morse_code = coder.encode(text)
+        print(morse_code)
 
-        return render_template("index.html", morse_code=morse_code)
+        return render_template("index.html", form=form, morse_code=morse_code)
 
-    return render_template("index.html")
+    return render_template("index.html", form=form)
 
 
 if __name__ == "__main__":
