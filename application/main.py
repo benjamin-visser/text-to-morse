1fcca6e4768d1cabf1bfa642ca55be47f555de7b
Benjamin Visser <benjaminjamesvisser@gmail.com>
2022-02-11 09:47:16 +0200

    add input and output functionality to home page

--- application/main.py
+++ application/main.py
@@ -1,6 +1,5 @@
 from morse_coder import MorseCoder
 
-
 if __name__ == "__main__":
 
     coder = MorseCoder()
--- application/server.py
+++ application/server.py
@@ -1,11 +1,18 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request, redirect
+from morse_coder import MorseCoder
 
 app = Flask(__name__)
+coder = MorseCoder()
 
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def home():
+    if request.method == "POST":
+        text = request.form["user_text"]
+        morse_code = coder.encode(text)
+
+        return render_template("index.html", morse_code=morse_code)
+
     return render_template("index.html")
 
 
